aaaaac988dd497bd213abe1ff248ee45ed222ff0
Amadu <ndongamadou@gmail.com>
2017-04-20 13:42:50 +0000

    plotting aspects changes

--- js/code.js
+++ js/code.js
@@ -27,22 +27,8 @@ Highcharts.chart('container', {
             format: '{value}'
         },
         // min:5,
-        plotLines: [{
-            color: 'black',
-            dashStyle: 'dot',
-            width: 2,
-            value: 5,
-            value: 0,
-            label: {
-                rotation: 0,
-                y: 10,
-                style: {
-                    fontStyle: 'italic'
-                },
-                // text: 'Number of reported sexual violence'
-            },
-            zIndex: 3
-        }]
+        categories: countries,
+
     },
 
     yAxis: {
@@ -55,19 +41,6 @@ Highcharts.chart('container', {
             format: '{value}'
         },
         maxPadding: 0.2,
-        plotLines: [{
-            color: 'black',
-            dashStyle: 'dot',
-            width: 2,
-            value: 5,
-            label: {
-                align: 'right',
-                style: {
-                    fontStyle: 'italic'
-                },
-            },
-            zIndex: 3
-        }]
     },
 
     tooltip: {
@@ -77,8 +50,8 @@ Highcharts.chart('container', {
             '<tr><th>Total SV reported :</th><td>{point.x}</td></tr>'+
             '<tr><th>Rape :</th><td>{point.rape}</td></tr>'+
             '<tr><th>Sexual torture :</th><td>{point.sexual_torture}</td></tr>'+
-            '<tr><th>Sexual slavery :</th><td>{point.sexual_slavery}</td></tr>'+
-            '<tr><th>Other sexual violence form :</th><td>{point.undetermined}</td></tr>',
+            '<tr><th>Sexual slavery :</th><td>{point.sexual_slavery}</td></tr>',
+            // '<tr><th>Other sexual violence form :</th><td>{point.undetermined}</td></tr>',
         footerFormat: '</table>',
         followPointer: true
     },
